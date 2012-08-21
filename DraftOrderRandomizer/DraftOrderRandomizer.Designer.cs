8ecb266d40a62afcb1a5ce18141e227a717ad62d
Brett Rogers <brettrogers@gmail.com>
2012-08-21 16:09:25 -0600

    Initial project commit

---
+++ DraftOrderRandomizer/DraftOrderRandomizer.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DraftRandomizer
+{
+    partial class DraftOrderRandomizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRandomize = new System.Windows.Forms.Button();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRandomize
+            // 
+            this.btnRandomize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRandomize.Location = new System.Drawing.Point(16, 303);
+            this.btnRandomize.Name = "btnRandomize";
+            this.btnRandomize.Size = new System.Drawing.Size(81, 23);
+            this.btnRandomize.TabIndex = 0;
+            this.btnRandomize.Text = "Randomize";
+            this.btnRandomize.UseVisualStyleBackColor = true;
+            this.btnRandomize.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtList
+            // 
+            this.txtList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtList.Location = new System.Drawing.Point(16, 78);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtList.Size = new System.Drawing.Size(213, 215);
+            this.txtList.TabIndex = 2;
+            this.txtList.Text = "Team 1\r\nTeam 2\r\nTeam 3\r\nTeam 4\r\nTeam 5\r\nTeam 6\r\nTeam 7\r\nTeam 8\r\nTeam 9\r\nTeam 10\r\n" +
+    "Team 11\r\nTeam 12";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblInstructions.Location = new System.Drawing.Point(13, 13);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(216, 47);
+            this.lblInstructions.TabIndex = 3;
+            this.lblInstructions.Text = "Enter the items to be randomized, separated by hard returns. Results are copied t" +
+    "o the clipboard.";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(153, 302);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // DraftOrderRandomizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 338);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.btnRandomize);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(258, 376);
+            this.Name = "DraftOrderRandomizer";
+            this.ShowIcon = false;
+            this.Text = "Draft Order Randomizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRandomize;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ DraftOrderRandomizer/DraftOrderRandomizer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DraftRandomizer
+{
+    public partial class DraftOrderRandomizer : Form
+    {
+        public DraftOrderRandomizer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            string[] initialOwnerArray;
+            string[] splitArray = new string[1];
+            System.Collections.ArrayList list = new System.Collections.ArrayList();
+            int length;
+            int index;
+                        
+            splitArray[0] = Environment.NewLine; // Used to split the list of owners in the textbox
+
+            // Add contents of textbox to array
+            list.AddRange(initialOwnerArray = txtList.Text.Split(splitArray, StringSplitOptions.RemoveEmptyEntries));
+            
+            txtList.Clear();
+
+            length = list.Count;
+
+            // Loop through list, randomly selecting an item, adding it to the output list, 
+            // then removing it from the list
+            for (int x = 0; x < length; x++)
+            {
+                index = random.Next(list.Count);
+
+                txtList.Text +=  list[index].ToString() + Environment.NewLine;
+                list.Remove(list[index].ToString());
+            }
+
+            // Add output to clipboard 
+            if (!String.IsNullOrEmpty(txtList.Text))
+                Clipboard.SetText(txtList.Text);           
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtList.Clear();
+        }
+    }
+}
