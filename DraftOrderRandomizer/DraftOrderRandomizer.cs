51633d212130530fc80cfd9b66256ed8a2ebd5e8
Brett Rogers <brett@noxad.com>
2014-07-10 00:08:21 -0700

    Resharper cleanup

--- DraftOrderRandomizer/DraftOrderRandomizer.cs
+++ DraftOrderRandomizer/DraftOrderRandomizer.cs
@@ -12,29 +12,26 @@ namespace DraftOrderRandomizer
 
         private void btnRandomize_Click(object sender, EventArgs e)
         {
-            Random random = new Random();
-            string[] initialArray;
-            string[] splitArray = new string[1];
-            System.Collections.ArrayList list = new System.Collections.ArrayList();
-            int length;
-            int index;
-                        
+            var random = new Random();
+            var splitArray = new string[1];
+            var list = new System.Collections.ArrayList();
+
             splitArray[0] = Environment.NewLine; // Used to split the list of owners in the textbox
 
             // Add contents of textbox to array
-            list.AddRange(initialArray = txtList.Text.Split(splitArray, StringSplitOptions.RemoveEmptyEntries));
+            list.AddRange(txtList.Text.Split(splitArray, StringSplitOptions.RemoveEmptyEntries));
             
             txtList.Clear();
 
-            length = list.Count;
+            var length = list.Count;
 
             // Loop through list, randomly selecting an item, adding it to the output list, 
             // then removing it from the list
-            for (int x = 0; x < length; x++)
+            for (var x = 0; x < length; x++)
             {
-                index = random.Next(list.Count);
+                var index = random.Next(list.Count);
 
-                txtList.Text +=  list[index].ToString() + Environment.NewLine;
+                txtList.Text +=  list[index] + Environment.NewLine;
                 list.Remove(list[index].ToString());
             }
 
